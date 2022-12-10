ad78794ca72c290695d7335342d2f5c0d8bb05fa
camptein <106569137+camptein@users.noreply.github.com>
2022-12-11 03:55:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.end('Hello Docker');
-})
-
-server.listen(PORT, () => {
-    console.log('Running on http://localhost:');
-});
